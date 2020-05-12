62fa7a1d052281a4ae680021dd1f06529936272d
Nikii1118 <47010164+Nikii1118@users.noreply.github.com>
2020-05-11 23:09:38 -0500

    LAB Assignment-2

---
+++ Android Lab/Part A/Source/app/src/main/java/com/example/login_lab2/UserProfile.java
@@ -0,0 +1,95 @@
+package com.example.login_lab2;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.InputStream;
+
+public class UserProfile extends AppCompatActivity {
+
+    private ShareDialog shareDialog;
+    private Button logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this);
+        setContentView(R.layout.activity_userprofile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        shareDialog = new ShareDialog(this);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                ShareLinkContent content = new ShareLinkContent.Builder().build();
+                shareDialog.show(content);
+            }
+        });
+
+        Bundle inBundle = getIntent().getExtras();
+        String name = inBundle.get("name").toString();
+        String surname = inBundle.get("surname").toString();
+        String imageUrl = inBundle.get("imageUrl").toString();
+
+        TextView nameView = (TextView) findViewById(R.id.nameAndSurname);
+        nameView.setText("" + name + " " + surname);
+        Button logout = (Button) findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                LoginManager.getInstance().logOut();
+                Intent login = new Intent(UserProfile.this, MainActivity.class);
+                startActivity(login);
+                finish();
+            }
+        });
+        new UserProfile.DownloadImage((ImageView)findViewById(R.id.profileImage)).execute(imageUrl);
+    }
+
+    public class DownloadImage extends AsyncTask<String, Void, Bitmap>{
+        ImageView bmImage;
+
+        DownloadImage(ImageView bmImage){
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls){
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try{
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            }catch (Exception e){
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result){
+            bmImage.setImageBitmap(result);
+        }
+
+    }
+}
